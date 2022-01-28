2e7d6d560df995907f72f5b612815808db988759
zacharesmer <53238192+zacharesmer@users.noreply.github.com>
2022-01-28 12:05:02 -0500

    Add instructions for the student to print their name

--- src/main/java/bsu/comp152/Main.java
+++ src/main/java/bsu/comp152/Main.java
@@ -3,6 +3,6 @@ package bsu.comp152;
 public class Main {
     public static void main(String[] args) {
         // Example homework starter code
-        // TODO: print "Hello World!"
+        // TODO: print "Hello World! This program was written by <your name>"
     }
 }
