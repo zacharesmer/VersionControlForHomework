d18098440410efeece6d39d4b48b35fafffe7e71
zacharesmer <zachresmer@gmail.com>
2022-01-28 13:24:22 -0500

    Add fruit printing functionality

--- src/main/java/bsu/comp152/Main.java
+++ src/main/java/bsu/comp152/Main.java
@@ -1,8 +1,18 @@
 package bsu.comp152;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         // Example homework starter code
         // TODO: print "Hello World! This program was written by <your name>"
+
+        //  We can both add a different fruit to this list and see a merge conflict:
+        List<String> fruits = Arrays.asList("pineapple", "strawberry", "mango");
+        for (String f : fruits){
+            System.out.println(f);
+        }
     }
 }
